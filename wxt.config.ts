f05bfe8fe2c910bfc7e04c2963f1b79ebeecdc7b
4001-lab <salman.farsi.4001@gmail.com>
2026-04-07 22:59:57 +0500

    update extension key

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
     permissions: ['storage', 'identity'],
-    key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtMLtPabxfTYpukumq/WglkFKQlpQbMlJteDx1Z7VHHrgzvQxVKxnEIGSLulX+fpHQzJVPfIEKj4TmrNWWSR11YXmK0AusyAA1uQSl9W8iky8XOlWtxTxXEJa5VHBVvBQtEBipTMOYHtyivHsPogi2VWCwPplLlUuvYH47f3USjmsUpnFpNcf6QMfe11mw4OTocdsFrNPXtip9mtf81VVTsBJSkkBw9C4YAH9gf+A09kCwsG6ze1JvUoiJNycrOuyMdeMlBKkx/41KeVSijQCVymVTLbvho2mZC3TLFkePbaww57AjGNvWnzHZW9P5JBXLuZfP+V3AkKUdhSnHS5N0QIDAQAB',
+    key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0ByYjM6dJ0iwKwYcuUaA4sQw0KyFE/esvrAnqtu3Pcaq0zr8yPnkxlmTdbUCFTxwf/Y9crl445n1c1jEot4cwQ6hP7eDqnyJZWJIW9cK6iEdL8LXkigapqWd48fMQrEmiQneg8V1gBtJ69cwf7PvR1MrssDQhom9GIxPwU9DIFoiBn4IyYjaVcCkN/2j2rHXGxWEfg/PVchUvZcGaCeFYQjg155D2PQFjpwRFgphR1KIsxQdhk6DTkrIJFxBZm91PmeTCeiBMDGOSRHUo+qsGlKh0A+pBxVFXdB5AQis3PemDf0R414VWJCGZy6Fcb8b5o3IZEDSo7KKCa/+F4dRVQIDAQAB',
   },
 });
 
