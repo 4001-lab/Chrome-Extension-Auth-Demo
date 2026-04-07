b1b373228039945535019db686519414ba4a6945
4001-lab <salman.farsi.4001@gmail.com>
2026-04-07 20:55:44 +0500

    create permanent extension id

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
     permissions: ['storage', 'identity'],
-    //host_permissions: [],
+    key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtMLtPabxfTYpukumq/WglkFKQlpQbMlJteDx1Z7VHHrgzvQxVKxnEIGSLulX+fpHQzJVPfIEKj4TmrNWWSR11YXmK0AusyAA1uQSl9W8iky8XOlWtxTxXEJa5VHBVvBQtEBipTMOYHtyivHsPogi2VWCwPplLlUuvYH47f3USjmsUpnFpNcf6QMfe11mw4OTocdsFrNPXtip9mtf81VVTsBJSkkBw9C4YAH9gf+A09kCwsG6ze1JvUoiJNycrOuyMdeMlBKkx/41KeVSijQCVymVTLbvho2mZC3TLFkePbaww57AjGNvWnzHZW9P5JBXLuZfP+V3AkKUdhSnHS5N0QIDAQAB',
   },
 });
 
